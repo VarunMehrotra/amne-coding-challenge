f215a18e916523aca656a8eed5c95d4ead6a7416
Tim Arioto <timarioto@gmail.com>
2017-03-27 17:38:31 -0700

    Add stringToNumber, calculateSubrangeNumber, splitArrayIntoFixedWindows

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-window.onload = function() {
+window.onload = () => {
 		var fileInput = document.getElementById('fileInput');
 		var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+    const stringToNumber = (array) => {
+      let result = []
+      array.forEach(function(string) {
+        result.push(parseInt(string))
+      })
+      return result
+    }
+
+		const calculateSubrangeNumber = (windowOfHomeSalePrice) => {
+      subrangeNumber = 0
+      for (var i = 0; i < windowOfHomeSalePrice.length; i++) {
+        if (windowOfHomeSalePrice[i] < windowOfHomeSalePrice[i + 1]) {
+          subrangeNumber += 1;
+        } else if (windowOfHomeSalePrice[i] > windowOfHomeSalePrice[i + 1]) {
+          subrangeNumber -= 1;
+        }
+      }
+
+      if (windowOfHomeSalePrice.every(function(element, index, array) {
+        if (array[index + 1] === undefined) {
+          return element;
+        } else {
+          return element < array[index + 1];
+        }
+      })) {
+        subrangeNumber += 1;
+      }
+      return subrangeNumber;
+    }
+
+    const splitArrayIntoFixedWindows = (arrayOfHomeSalePrice, windowSize, numberOfWindows) => {
+      let arrayOfWindows = [];
+      for (var i = 0; i < numberOfWindows; i++) {
+        arrayOfWindows.push(arrayOfHomeSalePrice.slice(i, i + windowSize))
+      }
+      return arrayOfWindows;
+    }
 
 		fileInput.addEventListener('change', (e) => {
 			var file = fileInput.files[0];
@@ -9,12 +47,19 @@ window.onload = function() {
 			if (file.type.match(textType)) {
 				var reader = new FileReader();
 
-				reader.onload = function(e) {
+				reader.onload = (e) => {
           let firstLine = reader.result.split('\n')[0]
           let secondLine = reader.result.split('\n')[1]
-          console.log(firstLine)
-          console.log(secondLine)
-					fileDisplayArea.innerText = reader.result;
+
+          firstLineArray = stringToNumber(secondLine.split(' '))
+          secondLineArray = stringToNumber(secondLine.split(' '))
+
+          fixedWindowArrays = splitArrayIntoFixedWindows(secondLineArray, 3, 3)
+
+          for (var i = 0; i < fixedWindowArrays.length; i++) {
+            fileDisplayArea.innerHTML += '<div>' + calculateSubrangeNumber(fixedWindowArrays[i]) + '</div'
+          }
+
 				}
 
 				reader.readAsText(file);
